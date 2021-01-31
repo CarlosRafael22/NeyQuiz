dad07dc2358867bc23b25fd5e554c097f5474dde
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-31 20:44:53 -0300

    feat(Quiz): Create twitter anchor style

--- src/components/Quiz/styles.js
+++ src/components/Quiz/styles.js
@@ -7,4 +7,24 @@ export const ButtonStyle = styled.button`
     padding: 0.8rem;
     cursor: pointer;
     font-size: 18px;
+    width: 200px;
+`
+
+export const TwitterAnchor = styled.a`
+/* Style all font awesome icons */
+  display: block;
+  padding: 15px;
+  margin: 1.5rem auto;
+  border-radius: 0.2rem;
+  font-size: 16px;
+  width: 200px;
+  text-align: center;
+  text-decoration: none;
+  background: #55ACEE;
+  color: white;
+
+/* Add a hover effect if you want */
+&:hover {
+  opacity: 0.7;
+}
 `
