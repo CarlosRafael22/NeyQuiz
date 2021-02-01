f0c505d92788a1e2a4b6bd4a7d610ecbe4ef73c1
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-31 21:07:59 -0300

    style(utils): Change title

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,5 +1,5 @@
 export default {
     bg: 'https://uploads.metropoles.com/wp-content/uploads/2019/12/16173754/Neymar-psg-100.jpg',
-    title: 'QuizNey - O Quiz do Adulto Ney',
+    title: 'Ney Quiz - O Quiz do Adulto Ney',
     description: 'Conhece tão bem assim o Neymar no Twitter? Vamos ver se você se lembra de alguns...',
 }
